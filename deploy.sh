fce522c5e0c2c35f967dd05062db43c710d21a02
kyle <kyle@lyhdev.com>
2011-11-10 22:20:51 +0800

    updated

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
+git pull
 grails clean
 grails war
 sudo service tomcat6 stop
+sudo rm -rf /var/lib/tomcat6/webapps-contpub/ROOT.war
+sudo rm -rf /var/lib/tomcat6/webapps-contpub/ROOT
 sudo cp target/SimaQian.war /var/lib/tomcat6/webapps-contpub/ROOT.war
 sudo service tomcat6 start
 tail -f /var/lib/tomcat6/logs/catalina.out
