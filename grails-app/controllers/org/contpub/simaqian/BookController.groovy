6405dc0d76843075edb8b1bd28598bfb7509219a
lyhcode (Yan-hong Lin) <lyhcode@gmail.com>
2012-03-20 11:14:39 +0800

    development update

--- grails-app/controllers/org/contpub/simaqian/BookController.groovy
+++ grails-app/controllers/org/contpub/simaqian/BookController.groovy
@@ -207,8 +207,8 @@ class BookController {
 
             g2d.setRenderingHint( RenderingHints.KEY_TEXT_ANTIALIASING,
                             RenderingHints.VALUE_TEXT_ANTIALIAS_ON )
-            g2d.setFont(new Font("Adobe Heiti Std", Font.PLAIN, 12))  
-
+            //g2d.setFont(new Font("Adobe Heiti Std", Font.PLAIN, 12))
+            g2d.setFont(new Font("sans-serif", Font.PLAIN, 12))
 
             FontMetrics fm = g2d.getFontMetrics();
             int lineHeight = fm.getHeight()*0.75;
